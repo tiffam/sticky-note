7e43e2be76ecb4ba5327600b257be50abf53be73
alice <alesialim@gmail.com>
2018-12-16 20:03:03 +0800

    Add note creation functionality

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ let notes = localStorage.getItem("notes")
 let noteId = localStorage.getItem("noteId")
   ? JSON.parse(localStorage.getItem("noteId"))
   : 0;
+
+// when user click on submit button, the inputs will be saved into local storage and browser refreshes
+document.getElementById("submit").addEventListener("click", function() {
+  noteId++;
+  notes.push({
+    id: noteId,
+    title: document.getElementById("title").value,
+    note: document.getElementById("note").value
+  });
+  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("noteId", JSON.stringify(noteId));
+  console.log(notes);
+});
