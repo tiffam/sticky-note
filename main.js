e5895bf5ed765256e76098afaead5b67a57aa3c5
alice <alesialim@gmail.com>
2018-12-16 20:41:51 +0800

    Add display notes functionality to create and display notes

--- main.js
+++ main.js
@@ -19,3 +19,32 @@ document.getElementById("submit").addEventListener("click", function() {
   localStorage.setItem("noteId", JSON.stringify(noteId));
   console.log(notes);
 });
+
+//create and display notes from notes array
+let displayNotes = function(notes, idName) {
+  for (let i = 0; i < notes.length; i++) {
+    let div = document.createElement("div");
+    let iconDelete = document.createElement("i");
+    let iconSave = document.createElement("i");
+    let h5 = document.createElement("h5");
+    let para = document.createElement("p");
+    div.setAttribute("class", "note");
+    h5.setAttribute("contenteditable", "true");
+    para.setAttribute("contenteditable", "true");
+    iconDelete.setAttribute("class", "delete fas fa-minus-circle");
+    iconDelete.setAttribute("id", "delete" + notes[i].id);
+    iconSave.setAttribute("class", "save fas fa-save");
+    iconSave.setAttribute("id", "save" + notes[i].id);
+    h5.textContent = notes[i].title;
+    para.textContent = notes[i].note;
+
+    div.appendChild(iconDelete);
+    div.appendChild(iconSave);
+    div.appendChild(h5);
+    div.appendChild(para);
+    document.getElementById(idName).appendChild(div);
+  }
+};
+
+//to execute the code to display notes
+displayNotes(notes, "show-notes");
